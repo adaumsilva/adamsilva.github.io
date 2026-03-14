ee13c1570b63e5d9477714e45dbc42ed10a9ea85
Adam Silva <adaumsilva@gmail.com>
2026-03-14 13:20:17 -0400

    Fixed repo path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/adamsilva.github.io",
   images: {
     unoptimized: true,
   },
